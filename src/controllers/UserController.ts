ac7e81446e2c9fdc6d33bd11be86d64501e6ea75
Carlos Daniel <carlos.daniel.moura99@gmail.com>
2024-01-23 09:36:40 -0300

    ✨ feat:Adicionado funcao getAllUsers

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -49,6 +49,20 @@ class UserController {
       res.status(400).json({ error: "User already exists" });
     }
   }
+  async getAllUsers(req: Request, res: Response) {
+    try {
+      const users = await User.find({}, { password: 0 }).exec();
+
+      if (users.length === 0) {
+        return res.status(404).json({ error: "Users not found" });
+      }
+
+      res.status(200).json(users);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
 }
 
 export default UserController;
